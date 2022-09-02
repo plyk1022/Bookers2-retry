2548798c39d266cf85b03a126d0c09fd5950ea03
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-09-02 06:03:07 +0000

    [Update]エラーチェック・修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
   
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
