5d541756619d09c72a6fae728dda7206d9f9e9a1
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-09-09 06:04:41 +0000

    [Add]DM機能　ルーム作成まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
- has_many :books, dependent: :destroy
- has_many :book_comments, dependent: :destroy
- has_many :favorites, dependent: :destroy
- 
+  has_many :books, dependent: :destroy
+  
+  # コメント機能
+  has_many :book_comments, dependent: :destroy
+  
+  # いいね機能
+  has_many :favorites, dependent: :destroy
+  
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
+  
+  # DM機能
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
  
  
  has_one_attached :profile_image
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   end
   
   get 'search' => 'searches#search'
+  
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create, :index, :show]
 
   get 'home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
